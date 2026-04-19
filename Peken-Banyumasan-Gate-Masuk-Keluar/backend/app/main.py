957592c41ff8b56fdffd45a46019d056508aefe7
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2026-04-19 17:13:48 +0700

    feat:dashboard stats API using Supabase

--- Peken-Banyumasan-Gate-Masuk-Keluar/backend/app/main.py
+++ Peken-Banyumasan-Gate-Masuk-Keluar/backend/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.api.routes import test
 from app.api.routes import gate
+from app.api.routes import dashboard
+
 app = FastAPI()
 
 app.include_router(test.router)
 app.include_router(gate.router)
+app.include_router(dashboard.router)
 @app.get("/")
 def root():
     return {"message": "Backend is running 🚀"}
